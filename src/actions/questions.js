577e77772ea4290a6863952e28cdbce21f9dabf6
VIjayanand Settin <stvijayanand@hotmail.com>
2019-04-19 08:32:26 -0700

    feat: saveQuestion action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,33 @@
+import {saveQuestions, saveQuestionAnswers} from "../api"
+
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
+export const SAVE_QUESTION = "SAVE_QUESTION";
+export const SAVE_QUESTIONANSWER = "SAVE_QUESTIONANSWER";
 
 export function receiveQuestions(questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions
   };
+}
+
+const saveQuestion = question =>{
+  return {
+      type: SAVE_QUESTION,
+      question
+  };
+}
+
+export const handleSaveQuestion = (optionOneText, optionTwoText) =>{
+  return (dispatch, getState) => {
+      const {authedUser} = getState();
+
+      return saveQuestion({
+                  author: authedUser,
+                  optionOneText,
+                  optionTwoText
+              })
+              .then(question => dispatch(saveQuestion(question)))
+  }
+
 }
